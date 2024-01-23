2f479da233587dfb9245cc3fbf81a3d48d7da0e8
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-23 17:26:51 +0530

    Create reverseString.java
    Implemented reverse String

---
+++ reverseString.java
@@ -0,0 +1,20 @@
+class App
+  {
+public static void main(String,args[])
+    {
+String s1= "Agni";
+      String s2 ="";
+      int len = s1.length();
+      while(len>=0)
+        {
+len-=1;
+          s2+=s1.charAt(len);
+          
+          
+        }
+      System.out.println(s1);
+      
+      
+    }
+    
+  }
